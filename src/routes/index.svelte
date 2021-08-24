70b04409be747fee20b4b6e9ee22905db97f960e
andywildridge <andy.wildridge@gmail.com>
2021-08-24 21:51:20 +0100

    stripping out demo content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,15 +3,15 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	// import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Grampohone</title>
 </svelte:head>
 
 <section>
-	<h1>
+	<!--<h1>
 		<div class="welcome">
 			<picture>
 				<source srcset="svelte-welcome.webp" type="image/webp" />
@@ -26,7 +26,8 @@
 		try editing <strong>src/routes/index.svelte</strong>
 	</h2>
 
-	<Counter />
+	<Counter />-->
+	Grampohone
 </section>
 
 <style>
@@ -36,24 +37,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
